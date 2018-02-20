b3ac8932129fe71e0e13decbfcace83b54803470
Bogdan.Chicos <Bogdan.Chicos@vauban.ro>
2018-02-20 12:10:01 +0200

    Some changes made while learning.

--- src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
@@ -4,17 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.in28minutes.spring.basics.springin5steps.basic.BinarySearchImpl;
+
 @SpringBootApplication
-public class SpringIn5StepsApplication {
+public class SpringIn5StepsBasicApplication {
 
 	public static void main(String[] args) {
 
-		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsApplication.class,
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsBasicApplication.class,
 				args);
 		try {
 			BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+			BinarySearchImpl binarySearch2 = applicationContext.getBean(BinarySearchImpl.class);
 
+			System.out.println(binarySearch);
+			System.out.println(binarySearch2);
+			
 			int result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
+			binarySearch2.binarySearch(new int[] { 12, 4, 6 }, 3);
 			System.out.println(result);
 		} finally {
 			applicationContext.close();
