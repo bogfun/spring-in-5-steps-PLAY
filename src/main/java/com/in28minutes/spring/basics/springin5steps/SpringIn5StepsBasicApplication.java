18fbf0eca29a229f23257a290f2aecb986f38daf
Bogdan.Chicos <Bogdan.Chicos@vauban.ro>
2018-02-20 15:48:39 +0200

    Spring without SpringBoot. @Configuration & @ComponentScan

--- src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
@@ -1,30 +1,29 @@
 package com.in28minutes.spring.basics.springin5steps;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.in28minutes.spring.basics.springin5steps.basic.BinarySearchImpl;
 
 @Configuration
+@ComponentScan
 public class SpringIn5StepsBasicApplication {
 
 	public static void main(String[] args) {
 
-		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsBasicApplication.class,
-				args);
-		try {
-			BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
-			BinarySearchImpl binarySearch2 = applicationContext.getBean(BinarySearchImpl.class);
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+				SpringIn5StepsBasicApplication.class);
 
-			System.out.println(binarySearch);
-			System.out.println(binarySearch2);
-			
-			int result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
-			binarySearch2.binarySearch(new int[] { 12, 4, 6 }, 3);
-			System.out.println(result);
-		} finally {
-			applicationContext.close();
-		}
+		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+		BinarySearchImpl binarySearch2 = applicationContext.getBean(BinarySearchImpl.class);
+
+		System.out.println(binarySearch);
+		System.out.println(binarySearch2);
+
+		int result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
+		binarySearch2.binarySearch(new int[] { 12, 4, 6 }, 3);
+		System.out.println(result);
 	}
 }
