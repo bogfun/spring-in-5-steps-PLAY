578bad66cabbe461f18f2bd5bfdc1b41f0a6e596
Bogdan.Chicos <Bogdan.Chicos@vauban.ro>
2018-02-21 14:35:20 +0200

    Specific component annotation layer.

--- src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsScopeApplication.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsScopeApplication.java
@@ -8,6 +8,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import com.in28minutes.spring.basics.springin5steps.scope.PersonDAO;
 
+import utils.Utils;
+
 @SpringBootApplication
 public class SpringIn5StepsScopeApplication {
 
@@ -22,11 +24,12 @@ public class SpringIn5StepsScopeApplication {
 			PersonDAO personDao = applicationContext.getBean(PersonDAO.class);
 			PersonDAO personDao2 = applicationContext.getBean(PersonDAO.class);
 
+			Utils.startLog();
 			LOGGER.info("{}", personDao);
 			LOGGER.info("{}", personDao.getJdbcConnection());
 			LOGGER.info("{}", personDao2);;
 			LOGGER.info("{}", personDao2.getJdbcConnection());
-			
+			Utils.endLog();
 		} finally {
 			applicationContext.close();
 		}
