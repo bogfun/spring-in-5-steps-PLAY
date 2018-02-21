3bdb3bc304c3d7aaabe4aeff16db7ea0ad4a9595
Bogdan.Chicos <Bogdan.Chicos@vauban.ro>
2018-02-21 12:08:55 +0200

    Play. Play. Play.

--- src/main/java/com/in28minutes/spring/basics/springin5steps/scope/JdbcConnection.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/scope/JdbcConnection.java
@@ -1,5 +1,8 @@
 package com.in28minutes.spring.basics.springin5steps.scope;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class JdbcConnection {
 
 	public JdbcConnection() {
